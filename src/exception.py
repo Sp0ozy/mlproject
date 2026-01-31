3c5d90887309b9fcc7727d7d8ae0918eff978eab
sp0ozy <imolchanov210@gmail.com>
2026-01-31 22:58:26 +0100

    basic custom exception

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+import logger
 
 def error_message_detail(error, error_detail: sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -16,4 +17,8 @@ class CustomException(Exception):
         return self.error_message
     
 if __name__ == "__main__":
-    
+    try:
+        a = 1 / 0
+    except Exception as e:
+        logger.logging.info("Dividing by zero error caught")
+        raise CustomException(e, sys)
