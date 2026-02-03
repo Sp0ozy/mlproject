bf7f0097b2040bf731e1f665d0a6c3a972cc3f9c
sp0ozy <imolchanov210@gmail.com>
2026-02-03 12:29:08 +0100

    basic data transformation pipeline

---
+++ src/utils.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+import pandas as pd
+import numpy as np
+import dill
+
+from src.logger import logging
+from src.exception import CustomException
+
+def save_object(file_path, obj):
+    try:
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+        with open(file_path, 'wb') as file_obj:
+            dill.dump(obj, file_obj)
+        logging.info(f"Object saved successfully at {file_path}")
+
+    except Exception as e:
+        logging.error("Error saving object")
+        raise CustomException(e, sys)
