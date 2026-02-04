01ee8e35e41724cc6861c96cd7fcf2bba9072da8
sp0ozy <imolchanov210@gmail.com>
2026-02-04 21:37:01 +0100

    evaluate_model util added

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,8 @@
 import os
 import sys
 
-import pandas as pd
-import numpy as np
 import dill
+from sklearn.metrics import r2_score
 
 from src.logger import logging
 from src.exception import CustomException
@@ -19,3 +18,25 @@ def save_object(file_path, obj):
     except Exception as e:
         logging.error("Error saving object")
         raise CustomException(e, sys)
+    
+def evaluate_model(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        for i in range(len(models.values())):
+            models_name = list(models.values())[i]
+
+            models_name.fit(X_train, y_train)
+
+            y_test_pred = models_name.predict(X_test)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+    
+    except Exception as e:
+        logging.error("Error evaluating model")
+        raise CustomException(e, sys)
+
