7a75bf00117d91329fe55a65ed7f48c6bcb3b346
sp0ozy <imolchanov210@gmail.com>
2026-02-06 12:22:11 +0100

    setup.py fixed

--- setup.py
+++ setup.py
@@ -6,9 +6,10 @@ def get_requirements(path:str) -> list[str]:
     requirements = []
     with open(path, "r") as f:
         requirements = f.readlines()
+    requirements = [req.strip() for req in requirements]
     if HYPER_E_DOT in requirements:
         requirements.remove(HYPER_E_DOT)
-    return [req.strip() for req in requirements]
+    return [req for req in requirements if req]
 
 setup(
     name="mlproject",
